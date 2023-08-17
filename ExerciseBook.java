997ef8dd31cf979cc2c49e0f27026bbe4bc967d9
itwill <yojinwo11@gmail.com>
2023-08-17 14:11:23 +0900

    테스트

--- ExerciseBook.java
+++ ExerciseBook.java
@@ -2,18 +2,7 @@ public class ExerciseBook {
     
     public static void main(String[] args) {
         
-        int[] array; 
-        array = new int[]{1,2,3,4,5};
-
-       arrayMethod(new int[] {1,2,3,4,5});
-    }
-
-    static void arrayMethod (int[] array) {
-
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
-        
+        System.out.println("아이티윌 테스트");
     }
 
 
