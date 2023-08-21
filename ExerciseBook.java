e2c335eb476506c98d6dab2fcb0cb705d0bbd3f2
omelet2 <yojinwo11git@gmail.com>
2023-08-21 16:34:53 +0900

    아이티윌

--- ExerciseBook.java
+++ ExerciseBook.java
@@ -5,6 +5,7 @@ public class ExerciseBook {
         System.out.println("아이티윌 테스트");
          System.out.println("아이티윌 테스트2");
            System.out.println("아이티윌 테스트3");
+           System.out.println("아이티윌 테스트 230821");
     }
 
 
